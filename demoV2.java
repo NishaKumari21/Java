1497058dea4baa6fd5db3cf82a40d1df1da1c281
Nisha Kumari <129987725+NishaKumari21@users.noreply.github.com>
2025-02-04 23:01:37 +0530

    Add files via upload

---
+++ demoV2.java
@@ -0,0 +1,17 @@
+public class demoV2 {
+    public static void  main(String[]args){
+        System.out.println("Adding 3 numbers:");
+        int a=10;
+        int b=30;
+        int c=40;
+        System.out.println(a+b+c);
+        char ch='A';
+        float t=2.4f;
+        double o=2.44D;
+        System.out.println(ch);
+        System.out.println(t);
+        System.out.println(o);
+
+
+    }
+}
