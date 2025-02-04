a85ca9ef7fef3649194403c438fffdbcd84cda3f
Nisha Kumari <129987725+NishaKumari21@users.noreply.github.com>
2025-02-04 23:39:14 +0530

    Add files via upload

---
+++ demoFORloop.java
@@ -0,0 +1,10 @@
+public class demoFORloop {
+    public static void main(String[] args) {
+        // FOR LOOP
+        int i=0;
+        for(i=0;i<=10;i++){
+            System.out.println(i);
+        }
+    }
+    
+}
